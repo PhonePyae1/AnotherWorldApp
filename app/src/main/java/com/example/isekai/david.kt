35009f2498f28db6ecc11eb5136f7679b385d6d0
phonepyae1 <phonepyaekyawswar00@gmail.com>
2021-07-16 23:00:48 +0700

    All files

---
+++ app/src/main/java/com/example/isekai/david.kt
@@ -0,0 +1,11 @@
+package com.example.isekai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class david : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_david)
+    }
+}
